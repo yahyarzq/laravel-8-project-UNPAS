aa267d3e3b61a22a9ee6112e9ca67463ba5f51c6
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-10-02 08:56:02 +0700

    11. N+1 Problem

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,6 +2,7 @@
 
     
 @section('container')
+<h1 class="mb-5">{{ $title }}</h1>
     @foreach($posts as $post)
     <article class="mb-5 border-bottom pb-4">
         <h1><a href="/posts/{{ $post->slug }}"class="text-decoration-none">{{ $post->title }}</a></h1>
--- routes/web.php
+++ routes/web.php
@@ -47,16 +47,15 @@ Route::get('/categories', function(){
 });
 
 Route::get('categories/{category:slug}', function(Category $category){
-    return view('category',[
-        'title'=> $category->name,
-        'posts'=>$category->posts,
-        'category'=>$category->name
+    return view('posts',[
+        'title'=> "Post by Category : $category->name",
+        'posts'=>$category->posts->load('category','author'),
     ]);
 });
 
 Route::get('/authors/{author:username}',function(User $author){
-    return view('authors',[
-        'title'=> 'User Post',
-        'posts'=>$author->posts,
+    return view('posts',[
+        'title'=> "Post by $author->name",
+        'posts'=>$author->posts->load('category','author'),
     ]);
 });
