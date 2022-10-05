08ec4ead4904eba38c6332ce17604d5203c69bdf
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-10-05 23:11:17 +0700

    20. Update & Delete Post

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -7,12 +7,12 @@
     </div>
 
     @if (session()->has('success'))
-    <div class="alert alert-success" role="alert">
+    <div class="alert alert-success col-lg-8" role="alert">
       {{ session('success') }}
     </div>
     @endif
 
-    <div class="table-responsive">
+    <div class="table-responsive col-lg-8">
       <a href="/dashboard/posts/create" class="btn btn-info mb-3">Create New Post</a>
         <table class="table table-striped table-sm">
           <thead>
@@ -31,8 +31,12 @@
                 <td>{{ $post->category->name }}</td>
                 <td>
                     <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye" class="align-text-bottom"></span></a>
-                    <a href="/dashboard/posts/{{ $post->id }}" class="badge bg-warning"><span data-feather="edit" class="align-text-bottom"></span></a>
-                    <a href="/dashboard/posts/{{ $post->id }}" class="badge bg-danger"><span data-feather="x-circle" class="align-text-bottom"></span></a>
+                    <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit" class="align-text-bottom"></span></a>
+                    <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                      @method('delete')
+                      @csrf
+                      <button class="badge bg-danger border-0" onclick=" return confirm('Are you sure')"><span data-feather="x-circle" class="align-text-bottom"></span></button>
+                    </form>
                 </td>
               </tr>
             @endforeach
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -8,10 +8,13 @@
             <h1 class="mb-3">{{ $post->title }}</h1>
             <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to Post
             </a>
-            <a href="" class="btn btn-warning"><span data-feather="edit"></span> Update Post
+            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Update Post
             </a>
-            <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete Post
-            </a>
+            <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                @method('delete')
+                @csrf
+                <button class="btn btn-danger" onclick=" return confirm('Are you sure')"><span data-feather="x-circle" class="align-text-bottom"></span> Delete Post</button>
+              </form>
             <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid" alt="{{ $post->category->name }}" >
 
             <article class="my-3">
