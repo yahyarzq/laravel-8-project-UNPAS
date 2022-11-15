d18b09c23c3ce4be1a91afd2846e7eee933349d9
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-11-15 15:27:16 +0700

    23. Authorization

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\DashboardPostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
@@ -67,6 +68,7 @@ Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class,'checkS
 
 Route::resource('/dashboard/posts',DashboardPostController::class)->middleware('auth');
 
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
 
 
 // Route::get('categories/{category:slug}', function(Category $category){
